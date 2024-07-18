75df426d45c305afd0e9a99d11d26b6415c73268
Silky <lauraacevedo90744@gmail.com>
2024-07-18 19:08:19 -0400

    Add project files.

---
+++ src/Plugin.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using RWF;
+using RWF.Swagshit;
+using System.IO;
+using System.Collections.Generic;
+using RWF.FNFJSON;
+using System.Linq;
+using System.Collections;
+using System.Runtime.InteropServices;
+using Rewired.UI.ControlMapper;
+
+namespace faznews_mewmix
+{
+    [BepInPlugin(MOD_ID, "RWF: SIGHTING", "1.0.0")]
+    class Plugin : BaseUnityPlugin
+    {
+        private const string MOD_ID = "silky.rwf.sighting.herobrine.silkyver";
+
+        // Add hooks
+        public void OnEnable()
+        {
+
+            Debug.Log("RWF: SIGHITNG: PLUGIN LOADED ENABLED");
+
+            On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
+
+        }
+
+        /*
+
+        private void FunkinMenu_UpdateCameraTarget(FunkinMenu self)
+        {
+
+            if (self.SONG.Name != "brine") return;
+
+            self.cameraTarget = new(680, 350);
+
+        }
+
+        private void FunkinMenu_OnCreate(FunkinMenu self)
+        {
+            if (self.SONG.Name != "brine") return;
+
+            events_time = new()
+            {
+                17903f,
+                52663f,
+                64024f,
+                75385f,
+
+            };
+            events_scrollspeed = new()
+            {
+                1.2f,
+                0.8f,
+                1.1f,
+                1.2f,
+
+            };
+
+            self.dad.sprite.isVisible = self.hpIconP1.sprite.isVisible = false;
+
+    }
+
+        private void FunkinMenu_OnUpdatePost(FunkinMenu self)
+        {
+            if (self.SONG.Name != "brine") return;
+
+            if (Conductor.songPosition > 4645 && !self.dad.sprite.isVisible && !self.hpIconP1.sprite.isVisible)
+            {
+                self.dad.sprite.isVisible = self.hpIconP1.sprite.isVisible = true;
+            }
+
+            if (events_time.Count > 0 && events_time[0] < Conductor.songPosition)
+            {
+                self.noteSpeed = 2.8f * events_scrollspeed[0];
+
+                events_time.RemoveAt(0);
+                events_scrollspeed.RemoveAt(0);
+
+            }
+
+        }
+
+        private void FunkinMenu_OnEnemyHit(FunkinMenu self, RWF.Swagshit.Note daNote)
+        {
+
+            var healthLoss = 0.023f;
+
+            if (self.SONG.Name != "brine") return;
+            if (self.health > healthLoss * 2) self.health -= healthLoss;
+        }
+
+        */
+
+        // Load any resources, such as sprites or sounds
+        private void LoadResources(RainWorld rainWorld)
+        {
+            //Futile.atlasManager.LogAllElementNames();
+            Futile.atlasManager.LoadImage("funkin/images/sh_e05");
+
+            RWF_Plugin.songsMetaData.Add("brine", new("rwf_sighting.SightingSongScript", "SIGHTING", 9));
+        }
+        
+    }
+}
---
+++ src/SongScript.cs
@@ -0,0 +1,70 @@
+﻿using RWF;
+using RWF.Swagshit.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rwf_sighting
+{
+    public class SightingSongScript : SongScript
+    {
+
+        private List<float> events_time = new()
+        {
+            17903f,
+            52663f,
+            64024f,
+            75385f,
+
+        };
+        private List<float> events_scrollspeed = new()
+        {
+            1.2f,
+            0.8f,
+            1.1f,
+            1.2f,
+
+        };
+
+        public void onCameraUpdate(CameraMoveEvent e)
+        {
+            e.cancelled = true;
+            e.position = new(680, 350);
+        }
+
+        public void onCreatePost()
+        {
+            FunkinMenu.instance.dad.sprite.isVisible = FunkinMenu.instance.hpIconP1.sprite.isVisible = false;
+        }
+
+        public void onUpdatePost()
+        {
+            if (Conductor.songPosition > 4645 && !FunkinMenu.instance.dad.sprite.isVisible && !FunkinMenu.instance.hpIconP1.sprite.isVisible)
+            {
+                FunkinMenu.instance.dad.sprite.isVisible = FunkinMenu.instance.hpIconP1.sprite.isVisible = true;
+            }
+
+            if (events_time.Count > 0 && events_time[0] < Conductor.songPosition)
+            {
+                FunkinMenu.instance.noteSpeed = 2.8f * events_scrollspeed[0];
+
+                events_time.RemoveAt(0);
+                events_scrollspeed.RemoveAt(0);
+
+            }
+        }
+
+        public void oNNoteHit(NoteEvent e)
+        {
+            if (!e.Note.mustPress)
+            {
+                var healthLoss = 0.023f;
+
+                if (FunkinMenu.instance.health > healthLoss * 2) FunkinMenu.instance.health -= healthLoss;
+            }
+        }
+
+    }
+}
